7d7b0f2d2ce004a9b541c2b907527fa1f2c5f608
adnan <madnan.rafiq@outlook.com>
2026-04-14 21:58:21 -0400

    feat(sandbox): add FusionTooltip sandbox page

---
+++ Alis.Reactive.SandboxApp/Areas/Sandbox/Controllers/Components/Fusion/TooltipController.cs
@@ -0,0 +1,19 @@
+using Alis.Reactive.SandboxApp.Areas.Sandbox.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alis.Reactive.SandboxApp.Areas.Sandbox.Controllers.Components.Fusion
+{
+    [Area("Sandbox")]
+    [Route("Sandbox/Components/Tooltip")]
+    public class TooltipController : Controller
+    {
+        [HttpGet("")]
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return View(
+                "~/Areas/Sandbox/Views/Components/Fusion/Tooltip/Index.cshtml",
+                new TooltipModel());
+        }
+    }
+}
